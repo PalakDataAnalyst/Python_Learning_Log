a8b80f63af5c30c7ac9cc3ce9617b370afd6a219
Palak Srivastava <palaksrivastava630@gmail.com>
2026-02-11 23:22:38 +0530

    Added program for single and multiple variables

---
+++ DAY2 Single and multiple variables.py
@@ -0,0 +1,12 @@
+DAY 2 - PROGRAM FOR SINGLE AND MULTIPLE VARIABLES
+
+previous_price = 65
+current_price = 60
+print(previous_price, current_price)
+print("Previous price of product:", previous_price, 
+"\nCurrent price of product:", current_price)
+
+price = 65
+print("Previus price of Product:", price)
+price = 60
+print("Current price of Product:", price)
